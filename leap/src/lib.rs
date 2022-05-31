67a1f597967e1f96c614a20e017ef41692cd6a0f
art-santos <arthursantosdeveloper@gmail.com>
2022-05-31 13:04:32 -0300

    chore: leap

---
+++ leap/src/lib.rs
@@ -0,0 +1,7 @@
+pub fn is_leap_year(year: u64) -> bool {
+    match year {
+        _ if year % 4 == 0 && year % 100 != 0 => true,
+        _ if year % 4 == 0 && year % 100 == 0 && year % 400 == 0 => true,
+        _ => false,
+    }
+}
